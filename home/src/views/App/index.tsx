01db99484973a2be51cf27906e357ad1e5a75e7d
guosheng <guosheng@mchz.com.cn>
2022-12-26 16:22:37 +0800

    feat: 删除无用代码

--- home/src/views/App/index.tsx
+++ home/src/views/App/index.tsx
@@ -23,23 +23,7 @@ export default () => {
     ]
   };
   const ref = useRef<ChartsRefInstance>();
-  // useEffect(() => {
-  //   fetch('https://github.com/users/GuoSTZ/contributions?from=2021-12-01&to=2021-12-31', {
-  //     mode: "no-cors",
-  //     headers: {
-  //       "Access-Control-Allow-Origin": "*",
-  //       "Content-Type": "text/html"
-  //     }
-  //   })
-  //     .then((res: Response) => {
-  //       console.log(res, res.formData, '====res')
-  //       // return res.json()
-  //       return res;
-  //     })
-  //     .then(data => {
-  //       console.log(data, '=====data')
-  //     })
-  // }, [])
+
   return (
     <div>
       <Charts 
