bc34d49ad442c9e67e22fbd4c462f3c455d72424
guosheng <guosheng@mchz.com.cn>
2023-01-17 15:54:25 +0800

    feat: home模块调整

--- home/src/bootstrap.js
+++ home/src/bootstrap.js
@@ -1,14 +1,25 @@
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from '@/views/App'
+import { BrowserRouter, useRoutes } from 'react-router-dom';
+import Home from '@/views';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
+global.API_PREFIX = 'http://git.mchz.com.cn/api/v4';
+
+const App = () => {
+  return <Home />
+}
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ConfigProvider locale={zhCN}>
+        <App />
+      </ConfigProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
---
+++ home/src/views/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import './index.module.less';
+
+export default () => {
+
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
