d346fccba2a8c1fbfbfc8f1443d9e4a997073c5f
guosheng <540063557@qq.com>
2023-02-04 16:35:59 +0800

    feat: 样式文件调整

--- umi-app/src/routes.ts
+++ umi-app/src/routes.ts
@@ -1,6 +1,5 @@
 export default [
   { exact: true, path: '/', component: './src/pages/index.tsx' },
-  { exact: true, path: '/umiApp', component: './src/pages/index.tsx' },
-  { exact: true, path: '/umiApp/user', component: './src/pages/users.tsx' },
-  { exact: true, path: '/umiApp/test', component: './src/pages/test.tsx' },
+  { exact: true, path: '/user', component: './src/pages/users.tsx' },
+  { exact: true, path: '/test', component: './src/pages/test.tsx' },
 ]
