230402f3fbbd7e8280e835d5c8da555e20873e0e
guosheng <guosheng@mchz.com.cn>
2023-02-07 19:25:07 +0800

    feat: 新增vue模块

---
+++ vue-app/vue.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  assetsDir: "static",
+  runtimeCompiler: true,
+
+  devServer: {
+    host: "0.0.0.0",
+    // 端口号
+    port: 8093,
+    headers: { //micro 跨域设置
+      'Access-Control-Allow-Origin': '*',
+    },
+    https: false,
+    // https:{type:Boolean}
+    //配置自动启动浏览器
+    open: true,
+    //热更新
+    hot: true,
+    //配置多个跨域
+    proxy: { }
+    // proxy: proxyConfig.proxyList,
+  },
+});
