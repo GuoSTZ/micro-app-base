6eb856b121cba29c39e4d57949e60ecdcf7cf8ca
guosheng <guosheng@mchz.com.cn>
2023-03-13 14:34:37 +0800

    feat: 目录调整

--- umi-app/src/pages/test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <div>test 页面</div>
-  )
-}
--- umi-app/src/routes.ts
+++
@@ -1,5 +0,0 @@
-export default [
-  { exact: true, path: '/', component: './src/pages/index.tsx' },
-  { exact: true, path: '/user', component: './src/pages/users.tsx' },
-  { exact: true, path: '/test', component: './src/pages/test.tsx' },
-]
