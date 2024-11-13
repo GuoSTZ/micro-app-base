9b5231aa7ab4e35dd224c6cb99481557cf33d0c8
guosheng <guosheng@mchz.com.cn>
2024-11-13 15:32:36 +0800

    feat: test2

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,24 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React@18.2.0 + Webpack@5.75.0
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  if(true) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            React@18.2.0 + Webpack@5.75.0
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default App;
