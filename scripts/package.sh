ebc4ff7314852ca37276f2769d500b144064c6a1
guosheng <guosheng@mchz.com.cn>
2024-11-20 15:45:41 +0800

    feat: 目录调整

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,7 +6,7 @@ FILE_NAME=micro-app-base.tgz
 rm -rf $FTP_BUILD
 mkdir -p $FTP_BUILD
 
-for i in `ls -F | grep '/$' | grep -v 'bash/' | grep -v 'node_modules/' | grep -v $(echo "$FTP_BUILD") | grep -v 'components/'`
+for i in `ls -F | grep '/$' | grep -v 'scripts/' | grep -v 'node_modules/' | grep -v $(echo "$FTP_BUILD") | grep -v 'components/'`
 do
   cp -r $i/build $FTP_BUILD/$(echo "$i")
 done
