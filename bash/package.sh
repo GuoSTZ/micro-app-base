0988fc35b4f5987e3cd4627eb8eaf0dfeb744171
guosheng <guosheng@mchz.com.cn>
2024-11-20 11:00:59 +0800

    feat: 仓库同步设置

---
+++ bash/package.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FTP_BUILD=build
+FILE_NAME=micro-app-base.tgz
+
+rm -rf $FTP_BUILD
+mkdir -p $FTP_BUILD
+
+for i in `ls -F | grep '/$' | grep -v 'bash/' | grep -v 'node_modules/' | grep -v 'build/' | grep -v 'components/'`
+do
+  cp -r $i/build $FTP_BUILD/$(echo "$i")
+done
+
+# 包压缩
+cd $FTP_BUILD
+tar -czf ../$FILE_NAME .
+mv ../$FILE_NAME ./
+find . ! -name $FILE_NAME -delete
+cd -
